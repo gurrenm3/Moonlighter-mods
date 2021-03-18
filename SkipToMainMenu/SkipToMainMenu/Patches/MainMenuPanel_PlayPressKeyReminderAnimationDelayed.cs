a9462e5f1ad5ab6a6a87be948cd5014564ca2e2f
Thomas <thomaspmar@protonmail.com>
2021-03-17 17:57:19 -0700

    Added SkipToMainMenu mod

---
+++ SkipToMainMenu/SkipToMainMenu/Patches/MainMenuPanel_PlayPressKeyReminderAnimationDelayed.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Moonlighter_Practice_Mod.Patches
+{
+    [HarmonyPatch(typeof(MainMenuPanel), nameof(MainMenuPanel.PlayPressKeyReminderAnimationDelayed))]
+    internal class MainMenuPanel_PlayPressKeyReminderAnimationDelayed
+    {
+        [HarmonyPostfix]
+        internal static void Postfix(MainMenuPanel __instance)
+        {
+            __instance.PlayOpenAnimation();
+            __instance.StopPressKeyReminderAnimation();
+        }
+    }
+}
---
+++ SkipToMainMenu/SkipToMainMenu/Patches/MainMenuPanel_ShowGamepadGUI.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Moonlighter_Practice_Mod.Patches
+{
+    [HarmonyPatch(typeof(MainMenuPanel), nameof(MainMenuPanel.ShowGamepadGUI))]
+    internal class MainMenuPanel_ShowGamepadGUI
+    {
+        [HarmonyPostfix]
+        internal static void Postfix(MainMenuPanel __instance)
+        {
+            __instance.CloseGamepadPanel();
+        }
+    }
+}
