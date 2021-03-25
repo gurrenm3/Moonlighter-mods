d2c8cca390b693d3503df2e71f01cbcf47e0cd0e
Thomas <thomaspmar@protonmail.com>
2021-03-24 20:07:24 -0700

    Created "Bigger Item Stacks"

---
+++ Bigger_ItemStacks/Bigger_ItemStacks/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bigger_ItemStacks
+{
+    class Constants
+    {
+        public const int maxStackSize = 999;
+    }
+}
---
+++ Bigger_ItemStacks/Bigger_ItemStacks/Patches/MainMenuPanel_Awake.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace Bigger_ItemStacks.Patches
+{
+    [HarmonyPatch(typeof(MainMenuPanel), nameof(MainMenuPanel.Awake))]
+    internal class MainMenuPanel_Enable
+    {
+        [HarmonyPostfix]
+        internal static void Postfix(MainMenuPanel __instance)
+        {
+            foreach (var item in ItemDatabase.GetItems())
+            {
+                if (item.maxStack != Constants.maxStackSize)
+                    item.maxStack = Constants.maxStackSize;
+
+                if (item.maxChestStack != Constants.maxStackSize)
+                    item.maxChestStack = Constants.maxStackSize;
+            }
+        }
+    }
+}
